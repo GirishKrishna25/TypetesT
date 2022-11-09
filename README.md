a79ec41b3522975f6871a9735897303fc5970452
Girish <i.girish.krishna@gmail.com>
2022-11-09 06:21:48 +0530

    lecture 1 done

---
+++ README.md
@@ -0,0 +1,12 @@
+# TypetesT
+
+## packgages installed
+
+styled-components
+mui
+random-words
+
+## context-api
+
+alert slider
+theme
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { GlobalStyles } from "./styles/global";
+
+function App() {
+  return (
+    <div>
+      <GlobalStyles />
+      <h1>TypetesT</h1>
+    </div>
+  );
+}
+
+export default App;
